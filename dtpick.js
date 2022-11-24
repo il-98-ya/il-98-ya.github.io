3c423011f83a6a7627d52f111a9fb4c7f826bfb8
il-98-ya <shevcorpgamestudio@gmail.com>
2022-11-24 06:45:02 +0300

    Add files via upload

---
+++ dtpick.js
@@ -0,0 +1,5 @@
+var dateEl = document.getElementById('date');
+var timeEl = document.getElementById('time');
+
+document.getElementById('date-output').innerHTML = dateEl.type === 'date';
+document.getElementById('time-output').innerHTML = timeEl.type === 'time';
